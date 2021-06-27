a77b6ce8e1adc993b8251de4dedfbb3d60cc38c2
Hoosier-Daddy <uttkarshbansal13@gmail.com>
2021-06-27 16:54:58 +0530

    feat - data fetch from API successful

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 
 import './App.css';
+import { useEffect, useState } from 'react';
+import useAPI from './hooks/useAPI';
+
 
 function App() {
+
+  const api = 'https://api.spacexdata.com/v3/launches';
+  const [launches, setLaunches] = useState([]);
+  const { newDocuments } = useAPI(api);
+
+  // loading data
+  useEffect(() => {
+
+    setLaunches(newDocuments);
+    // setFilteredLaunches(newDocuments);
+
+
+  }, [newDocuments]);
+
   return (
     <div className="App">
-
+      {console.log(launches)}
     </div>
   );
 }
