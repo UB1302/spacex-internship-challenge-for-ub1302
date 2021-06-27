f4754d980b8ac06020fe9320f265c672efa040d9
Hoosier-Daddy <uttkarshbansal13@gmail.com>
2021-06-27 17:48:27 +0530

    feat- created Loading and Error component

---
+++ src/components/Error.js
@@ -0,0 +1,11 @@
+
+const Error = () => {
+    return (
+
+        <div className="error-container">
+            <h1>No results found for the specified filter</h1>
+        </div>
+    )
+}
+
+export default Error;
